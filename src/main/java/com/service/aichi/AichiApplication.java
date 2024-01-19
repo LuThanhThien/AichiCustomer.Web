24ce5dbc1ffea67dc6993316bc880269e9519cdf
Lu Thanh Thien <luthien5921@gmail.com>
2024-01-19 11:35:22 +0700

    init git

---
+++ src/main/java/com/service/aichi/AichiApplication.java
@@ -0,0 +1,13 @@
+package com.service.aichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AichiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AichiApplication.class, args);
+	}
+
+}
